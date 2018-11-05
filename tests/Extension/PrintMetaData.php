914f94f950bef08fab3a85dda1bec2aac245ac0d
Benjamin Heisig <benjamin@heisig.name>
2018-11-05 15:52:51 +0100

    Print some information at the beginning of unit tests
    - URL to entry point of i-doit JSON-RPC API
    - Version of library

---
+++ tests/Extension/PrintMetaData.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright (C) 2016-18 Benjamin Heisig
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Benjamin Heisig <https://benjamin.heisig.name/>
+ * @copyright Copyright (C) 2016-18 Benjamin Heisig
+ * @license http://www.gnu.org/licenses/agpl-3.0 GNU Affero General Public License (AGPL)
+ * @link https://github.com/bheisig/i-doit-api-client-php
+ */
+
+declare(strict_types=1);
+
+namespace bheisig\idoitapi\tests\Extension;
+
+use PHPUnit\Runner\BeforeFirstTestHook;
+use Symfony\Component\Dotenv\Dotenv;
+
+final class PrintMetaData implements BeforeFirstTestHook {
+
+    public function executeBeforeFirstTest(): void {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../../.env');
+
+        $url = getenv('URL');
+
+        $composerFile = __DIR__ . '/../../composer.json';
+        $composer = json_decode(file_get_contents($composerFile), true);
+        $name = $composer['name'];
+        $version = $composer['version'];
+
+        fwrite(STDOUT, <<< EOF
+API URL:       $url
+Version:       $name $version
+
+
+EOF
+        );
+    }
+
+}
